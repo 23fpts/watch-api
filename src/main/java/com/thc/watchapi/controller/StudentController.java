2cf0169ff85ca8fe42382d48ca222be48ca95b39
唐浩诚 <120698463@qq.com>
2020-11-19 11:23:56 +0800

    test stu

---
+++ src/main/java/com/thc/watchapi/controller/StudentController.java
@@ -0,0 +1,8 @@
+package com.thc.watchapi.controller;/**  
+    * @Title: 
+    * @Package com.thc.watchapi.controller
+    * @Description: 
+    * @author thc
+    * @date 2020/11/19 11:23 上午
+    */public class StudentController  {
+}
