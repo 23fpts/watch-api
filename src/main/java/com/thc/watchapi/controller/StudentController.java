f041ae8cd2c87e7de48ce23dfe8f5e4b2bd72278
唐浩诚 <120698463@qq.com>
2020-11-19 11:24:03 +0800

    test stu

--- src/main/java/com/thc/watchapi/controller/StudentController.java
+++ src/main/java/com/thc/watchapi/controller/StudentController.java
@@ -1,8 +1,16 @@
-package com.thc.watchapi.controller;/**  
-    * @Title: 
-    * @Package com.thc.watchapi.controller
-    * @Description: 
-    * @author thc
-    * @date 2020/11/19 11:23 上午
-    */public class StudentController  {
+package com.thc.watchapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author thc
+ * @Title:
+ * @Package com.thc.watchapi.controller
+ * @Description:
+ * @date 2020/11/19 11:23 上午
+ */
+@RestController
+@RequestMapping("/api/v1/web/student")
+public class StudentController {
 }
